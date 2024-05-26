a6f89552f7ab1334f2c701b00071d0a0e98c9183
Azhar <azharshaikh33@gmail.com>
2024-05-26 04:18:38 +0400

    Auto Push

--- vars.tf
+++ vars.tf
@@ -1,1 +1,12 @@
 variable "ENV" {}
+variable "MYSQL_RDS_PORT" {}
+variable "MYSQL_RDS_STORAGE" {}
+variable "MYSQL_RDS_ENGINE_VERSION" {}
+variable "MYSQL_RDS_INSTANCE_TYPE" {}
+variable "DOCDB_PORT" {}
+variable "DOCDB_INSTANCE_CLASS" {}
+variable "DOCDB_INSTANCE_COUNT" {}
+variable "ELASTIC_CACHE_PORT" {}
+variable "ELASTIC_CACHE_NODE_TYPE" {}
+variable "ELASTIC_CACHE_NODE_COUNT" {}
+variable "ELASTIC_CACHE_ENGINE_VERSION" {}
